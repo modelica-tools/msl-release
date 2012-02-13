f4fcb6d194829f2d4ecb6b32a43c47b02df74a16
dietmarw <dietmarw@7ce873d0-865f-4ce7-a662-4bb36ea78beb>
2012-02-13 16:07:45 +0000

    Enable svn export by default (but leave the git option in there)
    git-svn-id: https://svn.modelica.org/projects/Modelica/branches/tools@4869 7ce873d0-865f-4ce7-a662-4bb36ea78beb

--- genRelease.sh
+++ genRelease.sh
@@ -2,16 +2,16 @@
 tag=${1:?}
 currDir=`pwd`
 releaseDir="Modelica_${tag:1}"
-export releaseVer="`echo ${tag:1} |cut -d_ -f1`"
+export releaseVersion="`echo ${tag:1} |cut -d_ -f1`"
 export MODELICAPATH="/tmp/MSLrelease/$releaseDir"
 # Clean up of previous script runs
 rm -rf "/tmp/MSLrelease"
 mkdir -p "$MODELICAPATH"
 ## Export out a given release from the svn server (use this for keyword expansion)
-#svn export "https://svn.modelica.org/projects/Modelica/tags/$tag" "."
+svn export "https://svn.modelica.org/projects/Modelica/tags/$tag" "."
 ## Export using git (for testing)
-git archive --remote=$MODELICALIBRARIES $tag | tar -x -C "$MODELICAPATH"
-mv "$MODELICAPATH/Modelica" "$MODELICAPATH/Modelica $releaseVer"
+##git archive --remote=$MODELICALIBRARIES $tag | tar -x -C "$MODELICAPATH"
+#mv "$MODELICAPATH/Modelica" "$MODELICAPATH/Modelica $releaseVersion"
 ## Call Dymola to generate the HTML documentation
 ## CURRENTLY NOT WORKING
 ##mkdir -p Modelica/Resources/help # not yet created by Dymola (bug)
