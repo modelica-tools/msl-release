ec887ccc0bbf2ea286d9574c3bd62bd33701c6ac
dietmarw <dietmarw@7ce873d0-865f-4ce7-a662-4bb36ea78beb>
2012-02-13 16:07:30 +0000

    Improved the release script.
    Now also generating the correct folder name.
    
    git-svn-id: https://svn.modelica.org/projects/Modelica/branches/tools@4858 7ce873d0-865f-4ce7-a662-4bb36ea78beb

--- genRelease.sh
+++ genRelease.sh
@@ -1,11 +1,13 @@
-#!/bin/sh
+#!/usr/bin/env bash
 tag=${1:?}
 currDir=`pwd`
+releaseDir="Modelica ${tag:1}"
 mkdir -p /tmp/MSLrelease
 cd /tmp/MSLrelease
 # Export out a given release from the svn server
-svn export "https://svn.modelica.org/projects/Modelica/tags/$tag"
-cd "$tag"
+svn export "https://svn.modelica.org/projects/Modelica/tags/$tag" "$releaseDir"
 # let's call Dymola to generate the HTML documentation
-dymola $currDir/generateHTML.mos
-zip -r "../ModelicaStandardLibrary_$tag.zip" .
+cd "$releaseDir"
+mkdir Modelica/Resources/help # not yet created by Dymola (bug)
+dymola "$currDir/generateHTML.mos"
+zip -r "../ModelicaStandardLibrary_${tag}.zip" .
