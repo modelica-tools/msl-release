a14148684e42a4798c41ab293d100c8490c392fb
dietmarw <dietmarw@7ce873d0-865f-4ce7-a662-4bb36ea78beb>
2012-02-13 16:07:40 +0000

    Rename documentation generator and avoid spaces in file names for now.
    git-svn-id: https://svn.modelica.org/projects/Modelica/branches/tools@4865 7ce873d0-865f-4ce7-a662-4bb36ea78beb

--- genRelease.sh
+++ genRelease.sh
@@ -1,13 +1,28 @@
 #!/usr/bin/env bash
 tag=${1:?}
 currDir=`pwd`
-releaseDir="Modelica ${tag:1}"
-mkdir -p /tmp/MSLrelease
-cd /tmp/MSLrelease
-# Export out a given release from the svn server
-svn export "https://svn.modelica.org/projects/Modelica/tags/$tag" "$releaseDir"
-# let's call Dymola to generate the HTML documentation
-cd "$releaseDir"
-mkdir Modelica/Resources/help # not yet created by Dymola (bug)
-dymola "$currDir/generateHTML.mos"
-zip -r "../ModelicaStandardLibrary_${tag}.zip" .
+releaseDir="Modelica_${tag:1}"
+releaseVer="`echo ${tag:1} |cut -d_ -f1`"
+export MODELICAPATH="/tmp/MSLrelease/$releaseDir"
+# Clean up of previous script runs
+rm -rf "/tmp/MSLrelease"
+mkdir -p "$MODELICAPATH"
+## Export out a given release from the svn server
+#svn export "https://svn.modelica.org/projects/Modelica/tags/$tag" "."
+## Export using git
+git archive --remote=$MODELICALIBRARIES $tag | tar -x -C "$MODELICAPATH"
+## Call Dymola to generate the HTML documentation
+## CURRENTLY NOT WORKING
+##mkdir -p Modelica/Resources/help # not yet created by Dymola (bug)
+##dymola "$currDir/generateHTML.mos"
+#
+# Call OpenModelica to generate the HTML help files
+omc "$currDir/genDocOMC.mos"
+# Remove stuff that should not be part of the release:
+cd "$MODELICAPATH"
+rm -rf "ModelicaTest"
+# Let us only include the default ModelicaServices
+mv "ModelicaServices-Variants/Default/ModelicaServices" "ModelicaServices"
+rm -rf "ModelicaServices-Variants"
+mv "Modelica" "Modelica $releaseVer"
+zip -qrFS "$currDir/ModelicaStandardLibrary_${tag}.zip" .
