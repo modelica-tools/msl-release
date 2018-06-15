7281cfc89f29f92003cb21d338ebf46380110e20
Dietmar Winkler <dietmar.winkler@dwe.no>
2018-06-15 12:50:49 +0200

    Updated the options and define out dir

--- genRelease.sh
+++ genRelease.sh
@@ -1,54 +1,49 @@
 #!/usr/bin/env bash
 
-# Which version control system do we use, [git|svn]:
-vcs=git
 # In case of git you should have defined the $MODELICALIBRARIES location
 # either system wide or just right here:
 #MODELICALIBRARIES=/path/to/git-repo
 
-# Which HTML generator should we use, [omc|dymola]:
-gen=omc
+# Which HTML generator should we use, [omc|dymola|no]:
+genHTML=omc
 
 tag=${1:?}
 currDir=`pwd`
 releaseDir="Modelica_${tag:1}"
 export releaseVersion="`echo ${tag:1} |cut -d+ -f1`"
 export MODELICAPATH="/tmp/MSLrelease/$releaseDir"
+export outDir="$currDir/out"
 # Clean up of previous script runs
 rm -rf "/tmp/MSLrelease"
 
-if [ "$vcs" = "svn" ]; then
-    echo Exporting archive using SVN...
-    # Export of  a given release from the svn server (use this for keyword expansion)
-    svn export "https://svn.modelica.org/projects/Modelica/tags/$tag" "$MODELICAPATH"
+mkdir -p "$MODELICAPATH"
+echo "Exporting archive using Git (with keyword expansion and filtering)..."
+# Export using git
+git archive --remote=$MODELICALIBRARIES $tag | tar -x -C "$MODELICAPATH"
     echo "...finished."
-else
-    mkdir -p "$MODELICAPATH"
-    echo "Exporting archive using Git (for testing since no keyword expansion)..."
-    # Export using git (for testing)
-    git archive --remote=$MODELICALIBRARIES $tag | tar -x -C "$MODELICAPATH"
-    echo "...finished."
-fi
-# Creating directory structure with release numbers
+
+# Creating directory structure with release numbesr
 mv "$MODELICAPATH/Modelica" "$MODELICAPATH/Modelica $releaseVersion"
 mv "$MODELICAPATH/ModelicaServices" "$MODELICAPATH/ModelicaServices $releaseVersion"
 mv "$MODELICAPATH/ModelicaReference" "$MODELICAPATH/ModelicaReference $releaseVersion"
 
-if [ "$gen" = "omc" ]; then
+if [ "$genHTML" = "omc" ]; then
     echo "Calling OpenModelica to generate the HTML documentation..."
     # omc +showErrorMessages +d=failtrace "$currDir/genDocOMC.mos"
+    mkdir -p "$outDir"
+    cd "$outDir"
     omc "$currDir/genDocOMC.mos"
-else
+elif [ "$genHTML" = "dymola" ]; then
     echo "Calling Dymola to generate HTML documentaion (NOT WORKING YET)..."
     # mkdir -p Modelica/Resources/help # not yet created by Dymola (bug)
     # dymola "$currDir/genDocDymola.mos"
     echo "NO HTML documentation generated!"
+else
+    echo "Running without HTML generation"
 fi
 
 echo "Generating the zip file..."
-# Remove stuff that should not be part of the release:
 cd "$MODELICAPATH"
-rm -rf "ModelicaTest"
 # Create the release zip file
-zip -qrFS "$currDir/ModelicaStandardLibrary_${tag}.zip" .
+zip -qrFS "$outDir/ModelicaStandardLibrary_${tag}.zip" .
 echo "...finished."
